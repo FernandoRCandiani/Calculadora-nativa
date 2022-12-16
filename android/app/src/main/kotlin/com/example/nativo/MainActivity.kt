4ac77c418b0954b2e2692fcb2b1d076fee8cf955
Fernando Candiani <candianirfernando@gmail.com>
2022-12-16 11:31:12 -0300

    Trabalhando com Kotlin

---
+++ android/app/src/main/kotlin/com/example/nativo/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.nativo
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+class MainActivity: FlutterActivity() {
+
+  private val CHANNEL = "candiani.com.br/nativo"
+
+  override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+    super.configureFlutterEngine(flutterEngine)
+
+    MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+      call, result ->
+
+      if(call.method == "calcSum") {
+        val a = call.argument<Int>("a")?.toInt() ?: 0
+        val b = call.argument<Int>("b")?.toInt() ?: 0
+        result.success(a + b)
+      } else {
+        result.notImplemented()
+      }
+      
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _a = 0;
+  int _b = 0;
+  int _sum = 0;
+
+  Future<void> _calcSum() async {
+    const channel = MethodChannel('candiani.com.br/nativo');
+
+    try {
+      final sum = await channel.invokeMethod('calcSum', {"a": _a, "b": _b});
+      setState(() {
+        _sum = sum;
+      });
+    } on PlatformException {
+      setState(() {
+        _sum = 0;
+      });
+    }
+
+    setState(() {
+      _sum = _a + _b;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Nativo'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Soma... $_sum',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  setState(() {
+                    _a = int.tryParse(value) ?? 0;
+                  });
+                },
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  setState(() {
+                    _b = int.tryParse(value) ?? 0;
+                  });
+                },
+              ),
+              ElevatedButton(
+                onPressed: _calcSum,
+                child: Text('Somar'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
